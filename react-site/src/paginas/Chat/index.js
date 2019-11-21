a85309dc76a09e7a0b9039db1e89d7f97438c31c
denizafontineles <denizafontineles@outlook.com>
2019-11-21 16:55:40 -0300

    Aquisição da API de mensagens

--- react-site/src/paginas/Chat/index.js
+++ react-site/src/paginas/Chat/index.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import { getMensagens } from '../../service/base'
+import Mensagem from './componentes/Mensagem'
 
 import './styles.css'
 
-function Chat (){
-    return(
-    <section className='chat'>
-        <h2>Essa é a página de chat</h2>
-    </section>
-    )
+class Chat extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            mensagens: ''
+        }
+    }
+
+    componentDidMount(){
+        getMensagens()
+        .then(response =>{
+            this.setState({
+                mensagens: response.data
+            })
+        })
+        .catch(error =>{
+            console.error(error)
+        }
+        )
+    }
+
+    render() {
+         return(
+            <section className='chat'>
+            <h2>Mensagens</h2>
+            {this.state.mensagens.length > 0
+            ? this.state.mensagens.map(item => {
+                return <Mensagem mensagem={item} key={item.id} />
+            })
+            : <span>Carregando mensagens</span>
+        }
+            </section>
+        )
+    }
 }
 
 export default Chat
